c08e69760ad55f66dbfb78e3880664198411c084
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-01-21 11:56:06 +0100

    add docstrings

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,12 +1,49 @@
 from passlib.context import CryptContext
 
+import os
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
 
+# instantiate CryptContext class
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_hashed_password(password: str) -> str:
+    """
+    Hashes PWD
+
+    Takes in the plain text password provied and returns a hased version using the specified hashing algorithm
+
+    Parameters
+    ----------
+    password : str
+        Plain text password
+
+    Returns
+    -------
+    str
+        Hased version of the plain text password.
+    """
     return password_context.hash(password)
 
 
 def verify_password(password: str, hashed_pass: str) -> bool:
+    """
+    Verify provided password.
+
+    Takes in the provided password and matches it against hashed password in the database using CryptContext
+
+    Parameters
+    ----------
+    password : str
+        Password provided by the user
+    hashed_pass : str
+        Hashed password stored in the database
+
+    Returns
+    -------
+    bool
+        Indicator wether verification was successfull
+    """
     return password_context.verify(password, hashed_pass)
